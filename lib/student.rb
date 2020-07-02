09b2154aacc2eb5b61901a0d63a56244740bdaa4
Dmitry Sychev <axiumone@gmail.com>
2020-07-02 14:50:46 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -59,15 +59,15 @@ class Student
   end
 
   def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    sql = "UPDATE students SET name = ?, grade = ?, WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    student = self.new(id, name, grade)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
     student
   end
 
